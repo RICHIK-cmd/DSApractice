a69c00813d75549e5bb9767c99efa1089d1ace80
RICHIK <richikghoshb26@gmail.com>
2026-03-30 23:20:17 +0530

    pushing

---
+++ p13.c
@@ -0,0 +1,21 @@
+// palindrome using recursion
+#include <stdio.h>
+#include <string.h>
+
+int isPalindrome(char str[], int start, int end) {
+    if(start >= end)
+        return 1;
+
+    if(str[start] != str[end])
+        return 0;
+
+    return isPalindrome(str, start + 1, end - 1);
+}
+
+int main() {
+    char str[] = "madam";
+    if(isPalindrome(str, 0, strlen(str)-1))
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
+}
