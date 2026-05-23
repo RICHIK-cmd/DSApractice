fd4329e170241e1ce947a35eb46f77c83c8784b7
RICHIK <richikghoshb26@gmail.com>
2026-05-23 22:57:02 +0530

    Reverse a String Using Stack

---
+++ p56.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+char stack[100];
+int top = -1;
+
+
+void push(char ch) {
+    stack[++top] = ch;
+}
+
+
+char pop() {
+    return stack[top--];
+}
+
+int main() {
+
+    char str[100];
+
+    printf("Enter a string: ");
+    gets(str);
+
+    
+    for (int i = 0; i < strlen(str); i++) {
+        push(str[i]);
+    }
+
+    printf("Reversed string: ");
+
+    
+    while (top != -1) {
+        printf("%c", pop());
+    }
+
+    return 0;
+}
