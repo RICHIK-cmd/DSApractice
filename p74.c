356253a0b15cb5ecd54a243edf296efecda8170e
RICHIK <richikghoshb26@gmail.com>
2026-07-11 12:04:05 +0530

    Deleting last node, DLL

---
+++ p74.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *prev;
+    struct Node *next;
+};
+
+struct Node* createNode(int value)
+{
+    struct Node *newNode=(struct Node*)malloc(sizeof(struct Node));
+
+    newNode->data=value;
+    newNode->prev=NULL;
+    newNode->next=NULL;
+
+    return newNode;
+}
+
+void createList(struct Node **head)
+{
+    int n,value;
+
+    printf("Enter number of nodes: ");
+    scanf("%d",&n);
+
+    if(n<=0)
+        return;
+
+    printf("Enter node values:\n");
+
+    scanf("%d",&value);
+
+    *head=createNode(value);
+
+    struct Node *temp=*head;
+
+    for(int i=2;i<=n;i++)
+    {
+        scanf("%d",&value);
+
+        struct Node *newNode=createNode(value);
+
+        temp->next=newNode;
+        newNode->prev=temp;
+        temp=newNode;
+    }
+}
+
+void display(struct Node *head)
+{
+    while(head)
+    {
+        printf("%d ",head->data);
+        head=head->next;
+    }
+    printf("\n");
+}
+
+void deleteLast(struct Node **head)
+{
+    if(*head==NULL)
+    {
+        printf("List Empty\n");
+        return;
+    }
+
+    if((*head)->next==NULL)
+    {
+        free(*head);
+        *head=NULL;
+        return;
+    }
+
+    struct Node *temp=*head;
+
+    while(temp->next!=NULL)
+        temp=temp->next;
+
+    temp->prev->next=NULL;
+
+    free(temp);
+}
+
+int main()
+{
+    struct Node *head=NULL;
+
+    createList(&head);
+
+    printf("Original List:\n");
+    display(head);
+
+    deleteLast(&head);
+
+    printf("After deleting last node:\n");
+    display(head);
+
+    return 0;
+}
