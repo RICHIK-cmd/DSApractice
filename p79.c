26485bc1885a09158577d29f1b883430a393dfde
RICHIK <richikghoshb26@gmail.com>
2026-07-13 12:05:47 +0530

    REVERSE DLL

---
+++ p79.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *prev;
+    struct Node *next;
+};
+
+struct Node* createNode(int value)
+{
+    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+
+    newNode->data = value;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+
+    return newNode;
+}
+
+void insertEnd(struct Node **head, int value)
+{
+    struct Node *newNode = createNode(value);
+
+    if(*head == NULL)
+    {
+        *head = newNode;
+        return;
+    }
+
+    struct Node *temp = *head;
+
+    while(temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    newNode->prev = temp;
+}
+
+void display(struct Node *head)
+{
+    if(head == NULL)
+    {
+        printf("List is Empty.\n");
+        return;
+    }
+
+    printf("Doubly Linked List: ");
+
+    while(head != NULL)
+    {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+
+    printf("\n");
+}
+
+void reverseDLL(struct Node **head)
+{
+    struct Node *temp = NULL;
+    struct Node *current = *head;
+
+    while(current != NULL)
+    {
+        temp = current->prev;
+        current->prev = current->next;
+        current->next = temp;
+
+        current = current->prev;
+    }
+
+    if(temp != NULL)
+        *head = temp->prev;
+}
+
+int main()
+{
+    struct Node *head = NULL;
+
+    int n, value, i;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        printf("Enter value %d: ", i);
+        scanf("%d", &value);
+
+        insertEnd(&head, value);
+    }
+
+    printf("\nOriginal List:\n");
+    display(head);
+
+    reverseDLL(&head);
+
+    printf("\nReversed List:\n");
+    display(head);
+
+    return 0;
+}
