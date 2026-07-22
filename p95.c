5e39b162ccda388b88467beb0c2636ac9e45271e
RICHIK <richikghoshb26@gmail.com>
2026-07-22 20:45:35 +0530

    MIN SEARCH

---
+++ p95.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node *createList(int n)
+{
+    struct Node *head=NULL,*temp=NULL,*newNode;
+    int value;
+
+    for(int i=1;i<=n;i++)
+    {
+        newNode=(struct Node*)malloc(sizeof(struct Node));
+
+        printf("Enter data: ");
+        scanf("%d",&value);
+
+        newNode->data=value;
+
+        if(head==NULL)
+        {
+            head=temp=newNode;
+        }
+        else
+        {
+            temp->next=newNode;
+            temp=newNode;
+        }
+    }
+
+    temp->next=head;
+    return head;
+}
+
+void display(struct Node *head)
+{
+    if(head==NULL)
+        return;
+
+    struct Node *temp=head;
+
+    do
+    {
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+    while(temp!=head);
+
+    printf("\n");
+}
+
+struct Node* insertBeginning(struct Node *head,int value)
+{
+    struct Node *newNode=(struct Node*)malloc(sizeof(struct Node));
+    newNode->data=value;
+
+    if(head==NULL)
+    {
+        newNode->next=newNode;
+        return newNode;
+    }
+
+    struct Node *last=head;
+
+    while(last->next!=head)
+        last=last->next;
+
+    newNode->next=head;
+    last->next=newNode;
+    head=newNode;
+
+    return head;
+}
+
+int main()
+{
+    int n,value;
+    struct Node *head;
+
+    printf("Enter number of nodes: ");
+    scanf("%d",&n);
+
+    head=createList(n);
+
+    printf("Enter value: ");
+    scanf("%d",&value);
+
+    head=insertBeginning(head,value);
+
+    display(head);
+
+    return 0;
+}
