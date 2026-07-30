dd2ed4e0229505e936a01898d10f8c2d31dea857
RICHIK <richikghoshb26@gmail.com>
2026-07-30 12:34:50 +0530

    NTH FROM END

---
+++ p112.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node* createList(int n)
+{
+    struct Node *head = NULL, *temp = NULL, *newNode;
+    int value;
+
+    for(int i = 1; i <= n; i++)
+    {
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+
+        printf("Enter data for node %d: ", i);
+        scanf("%d", &value);
+
+        newNode->data = value;
+
+        if(head == NULL)
+        {
+            head = temp = newNode;
+            newNode->next = head;
+        }
+        else
+        {
+            temp->next = newNode;
+            temp = newNode;
+            temp->next = head;
+        }
+    }
+
+    return head;
+}
+
+void display(struct Node *head)
+{
+    if(head == NULL)
+    {
+        printf("List is Empty.\n");
+        return;
+    }
+
+    struct Node *temp = head;
+
+    printf("Circular Linked List: ");
+
+    do
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    while(temp != head);
+
+    printf("\n");
+}
+
+int countNodes(struct Node *head)
+{
+    if(head == NULL)
+        return 0;
+
+    int count = 0;
+    struct Node *temp = head;
+
+    do
+    {
+        count++;
+        temp = temp->next;
+    }
+    while(temp != head);
+
+    return count;
+}
+
+void findNthEnd(struct Node *head, int n)
+{
+    if(head == NULL)
+    {
+        printf("List is Empty.\n");
+        return;
+    }
+
+    int count = countNodes(head);
+
+    if(n < 1 || n > count)
+    {
+        printf("Invalid Position.\n");
+        return;
+    }
+
+    int pos = count - n + 1;
+
+    struct Node *temp = head;
+
+    for(int i = 1; i < pos; i++)
+        temp = temp->next;
+
+    printf("%dth Node from End = %d\n", n, temp->data);
+}
+
+int main()
+{
+    int size, n;
+    struct Node *head = NULL;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &size);
+
+    head = createList(size);
+
+    display(head);
+
+    printf("Enter N: ");
+    scanf("%d", &n);
+
+    findNthEnd(head, n);
+
+    return 0;
+}
